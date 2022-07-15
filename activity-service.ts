561ef8a7ee419d1522b3529cb2aeb99fe710f22b
miniflycn <miniflycn@justany.net>
2022-07-15 17:25:17 +0800

    use ts-proto

---
+++ activity-service.ts
@@ -0,0 +1,75 @@
+/* eslint-disable */
+import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+export const protobufPackage = 'activityservice';
+
+export interface ActivityRequest {
+  id: string;
+}
+
+export interface ActivityResponse {
+  offer: Offer[];
+}
+
+export interface Offer {
+  nid: string;
+  title: string;
+  pictUrl: string;
+  icons: Icon[];
+}
+
+export interface Icon {
+  type: string;
+  bgColor: string;
+  borderColor: string;
+  fontColor: string;
+  text: string;
+  source: string;
+}
+
+export const ACTIVITYSERVICE_PACKAGE_NAME = 'activityservice';
+
+export interface ActivityServiceClient {
+  call(request: ActivityRequest): Observable<ActivityResponse>;
+}
+
+export interface ActivityServiceController {
+  call(
+    request: ActivityRequest,
+  ):
+    | Promise<ActivityResponse>
+    | Observable<ActivityResponse>
+    | ActivityResponse;
+}
+
+export function ActivityServiceControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ['call'];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(
+        constructor.prototype,
+        method,
+      );
+      GrpcMethod('ActivityService', method)(
+        constructor.prototype[method],
+        method,
+        descriptor,
+      );
+    }
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(
+        constructor.prototype,
+        method,
+      );
+      GrpcStreamMethod('ActivityService', method)(
+        constructor.prototype[method],
+        method,
+        descriptor,
+      );
+    }
+  };
+}
+
+export const ACTIVITY_SERVICE_NAME = 'ActivityService';
